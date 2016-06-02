6592b0a5120475fc96800977f13d47e46dfeb288
Kolya Korobochkin <me@korobochkin.com>
2016-06-02 16:49:29 +0300

    option iterface

--- plugin/src/Admin/Settings/Prototypes/Options/Option.php
+++ plugin/src/Admin/Settings/Prototypes/Options/Option.php
@@ -1,7 +1,7 @@
 <?php
 namespace Korobochkin\WCMultiCurrency\Admin\Settings\Prototypes\Options;
 
-abstract class Option {
+abstract class Option implements OptionInterface {
 
 	private $name = null;
 
