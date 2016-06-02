84e7f366ad14aeae24496d6d64d12c0ac32f118a
Kolya Korobochkin <me@korobochkin.com>
2016-06-02 14:05:25 +0300

    page

---
+++ plugin/src/Admin/Settings/Departments/OptionsGeneral/General/Page.php
@@ -0,0 +1,21 @@
+<?php
+namespace Korobochkin\WCMultiCurrency\Admin\Settings\Departments\OptionsGeneral\General;
+
+use \Korobochkin\WCMultiCurrency\Plugin;
+use Korobochkin\WCMultiCurrency\Admin\Settings\Prototypes\Pages;
+
+class Page extends Pages\Page {
+
+	public function __construct() {
+		parent::__construct(
+			'options-general.php',
+			__( 'Multi Currency', Plugin::NAME ),
+			__( 'Multi Currency', Plugin::NAME ),
+			'manage_options',
+			'general',
+			array( $this, 'render' ),
+
+			Plugin::NAME . '-rates'
+		);
+	}
+}
